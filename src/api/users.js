6a9037f2da38c8a24167c5eb757b0c720fc2d46b
NandhiniRengan1-GEP <Nandhini.Rengan@gep.com>
2025-11-07 18:15:53 +0530

    Fix: TypeError - NULL_REFERENCE

---
+++ src/api/users.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const UserService = require('../services/userService');
+const { validateUser } = require('../utils/validators');
+
+const userService = new UserService();
+
+// BUG 1: Null reference error - accessing property of undefined
+router.get('/:id', async (req, res, next) => {
+  try {
+    const user = await userService.getUserById(req.params.id);
+    
+    // BUG: user might be null/undefined, but we access user.id without checking
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    
+    console.log(`Fetching profile for user: ${user.id}`);
+    
+    // This will throw: TypeError: Cannot read property 'id' of undefined
+    const profile = await userService.getUserProfile(user.id);
+    
+    res.json({
+      user,
+      profile
+    });
+  } catch (error) {
+    next(error);
+  }
+});
